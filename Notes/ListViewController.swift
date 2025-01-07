e400527e325a3b35e01acacb412873c44d9a3db3
Aleksey Yundov <yundoff@gmail.com>
2025-01-07 15:33:23 +0300

    Create tabBar&Color resources

---
+++ Notes/ListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ListViewController.swift
+//  Notes
+//
+//  Created by Aleksey Yundov on 07.01.2025.
+//
+
+import UIKit
+
+final class ListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()        
+    }
+}
---
+++ Notes/NoteViewController.swift
@@ -0,0 +1,15 @@
+//
+//  NoteViewController.swift
+//  Notes
+//
+//  Created by Aleksey Yundov on 07.01.2025.
+//
+
+import UIKit
+
+final class NoteViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
